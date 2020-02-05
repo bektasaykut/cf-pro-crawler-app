fc13a2df061aed1b75f77aaf2ddcc054eedeed73
eemektas <eemektas@gmail.com>
2020-02-05 18:04:02 +0300

    image file name format defined as product_code_(num)

--- cf_pro_crawler/cf_pro_crawler/pipelines.py
+++ cf_pro_crawler/cf_pro_crawler/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
 
 
 class CfProCrawlerPipeline(object):
     def process_item(self, item, spider):
-        return item
+        os.chdir('/home/zx/Desktop/cf-pro-crawler-app/cf_pro_crawler/product_images')
+
+        for num in range(len(item['images'])): 
+            if item['images'][num]['path']:
+            	new_image_name = item['product_code'][0] + '_{}.jpg'.format(num+1)
+            	new_image_path = 'full/' + new_image_name
+
+            	os.rename(item['images'][num]['path'], new_image_path)
