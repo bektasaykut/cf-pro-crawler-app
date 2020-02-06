867b9a0b1e45ab1093fac0189a00e1b4d33a5bfb
eemektas <eemektas@gmail.com>
2020-02-06 04:34:50 +0300

    intended tab correction

--- cf_pro_crawler/cf_pro_crawler/pipelines.py
+++ cf_pro_crawler/cf_pro_crawler/pipelines.py
@@ -8,12 +8,14 @@ import os
 
 
 class CfProCrawlerPipeline(object):
-    def process_item(self, item, spider):
-        os.chdir('/home/zx/Desktop/cf-pro-crawler-app/cf_pro_crawler/product_images')
+	def process_item(self, item, spider):
+		os.chdir('/home/zx/Desktop/cf-pro-crawler-app/cf_pro_crawler/product_images')
 
-        for num in range(len(item['images'])): 
-            if item['images'][num]['path']:
-            	new_image_name = item['product_code'][0] + '_{}.jpg'.format(num+1)
-            	new_image_path = 'full/' + new_image_name
+		for num in range(len(item['images'])): 
+			if item['images'][num]['path']:
+				new_image_name = item['product_code'][0] + '_{}.jpg'.format(num+1)
+				new_image_path = 'full/' + new_image_name
 
-            	os.rename(item['images'][num]['path'], new_image_path)
+				os.rename(item['images'][num]['path'], new_image_path)
+
+		return item
