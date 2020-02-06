e384d4a531dd0aeab0b91f68db2a68271629420a
eemektas <eemektas@gmail.com>
2020-02-06 04:33:54 +0300

    category number and name items added

--- cf_pro_crawler/cf_pro_crawler/items.py
+++ cf_pro_crawler/cf_pro_crawler/items.py
@@ -6,6 +6,7 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+#from cf_pro_crawler.product import ProductSpider
 
 
 class CfProCrawlerItem(scrapy.Item):
@@ -15,6 +16,9 @@ class CfProCrawlerItem(scrapy.Item):
     product_price = scrapy.Field()
     product_desc = scrapy.Field()
     product_url = scrapy.Field()
+    product_cat_nums = scrapy.Field()
+    product_cat_names = scrapy.Field()
 
     image_urls = scrapy.Field()
     images = scrapy.Field()
+
